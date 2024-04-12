6a563247487323ac4166ec8f08d81c742ab2ebb1
mridubhatnagar <mridubhatnagar4@gmail.com>
2024-04-12 23:23:34 +0530

    file to create gmail service object using creds

---
+++ auth.py
@@ -0,0 +1,35 @@
+# import the required libraries 
+from googleapiclient.discovery import build 
+from google_auth_oauthlib.flow import InstalledAppFlow 
+from google.auth.transport.requests import Request 
+import pickle 
+import os 
+
+
+def connect_to_gmail_api(SCOPES, filename):
+    creds = None
+
+    # The file token.pickle contains the user access token. 
+    # Check if it exists 
+    if os.path.exists('token.pickle'): 
+
+        # Read the token from the file and store it in the variable creds 
+        with open('token.pickle', 'rb') as token: 
+            creds = pickle.load(token)
+
+    # If credentials are not available or are invalid, ask the user to log in. 
+    if not creds or not creds.valid: 
+        if creds and creds.expired and creds.refresh_token: 
+            creds.refresh(Request()) 
+        else: 
+            flow = InstalledAppFlow.from_client_secrets_file(filename, SCOPES) 
+            creds = flow.run_local_server(port=0)
+            
+
+    # Save the access token in token.pickle file for the next run 
+    with open('token.pickle', 'wb') as token: 
+        pickle.dump(creds, token) 
+
+    # Connect to the Gmail API 
+    service = build('gmail', 'v1', credentials=creds)
+    return service 
